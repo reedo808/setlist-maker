b9b2139cccc784672e9933e861f38ff95b9c6937
Reed Fox <reed.m.fox@gmail.com>
2020-09-26 15:44:18 -0700

    updated to remove home and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    HomeComponent,
-    AboutComponent
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
